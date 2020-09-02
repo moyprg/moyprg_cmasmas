73fcd255ee929faed913bfc42891943b50436a0b
moyprg <moy30spl@gmail.com>
2020-09-02 02:36:37 -0500

    Se creo la rama rama2moyprg2

---
+++ rama2moyprg2.cpp
@@ -0,0 +1,16 @@
+//Udemy Progamación ATS
+
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+	
+	cout<<"Se creo una rama en github."<<endl;
+	cout<<"Esta rama se llama: rama2moyprg2"<<endl;
+	
+	getch();
+	return 0;
+}
